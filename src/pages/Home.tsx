6c50a065ac37b1865fe9da6182f8ca660367c0ee
GhabrielBender <ghabrielbmutti@gmail.com>
2021-07-07 12:48:31 -0300

    Project config

---
+++ src/pages/Home.tsx
@@ -0,0 +1,31 @@
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/google-icon.svg';
+
+export function Home(){
+  return(
+    <div>
+      <aside>
+        <img src={illustrationImg} alt="Ilustration about questions and answers"/>
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo-real</p>
+      </aside>
+      <main>
+        <div>
+          <img src={logoImg} alt="Pergunta Tchê"/>
+          <button>
+            <img src={googleIconImg} alt="Google image"/>
+            Crie sua sala com o google
+          </button>
+          <div>
+            ou entre em uma sala
+          </div>
+          <form>
+            <input type="text" placeholder="Digite o código da sala"></input>
+            <button type="submit">Entrar na sala</button>
+          </form>
+        </div>
+      </main>
+    </div>
+  )
+}
