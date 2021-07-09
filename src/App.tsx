4727fcfdeb9f28fabe1569a0d41094b5299b85bb
DESKTOP-0HLORID\ghabr <ghabrielbmutti@gmail.com>
2021-07-09 16:11:24 -0300

    Checking if the room exists and directing the user to the new room

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
-import { Home } from './pages/Home';
-function App(){
-  return(
-    <Home/>
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import { Room } from "./pages/Room";
+
+import { AuthContextProvider } from "./contexts/AuthContextProvider";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+        </Switch>
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,74 @@
-import illustrationImg from '../assets/images/illustration.svg';
-import logoImg from '../assets/images/logo.svg';
-import googleIconImg from '../assets/images/google-icon.svg';
+import { useHistory } from "react-router-dom";
 
-export function Home(){
-  return(
-    <div>
+import illustrationImg from "../assets/images/illustration.svg";
+import logoImg from "../assets/images/logo.svg";
+import googleIconImg from "../assets/images/google-icon.svg";
+
+import { Button } from "../components/Button";
+import { useAuth } from "../hooks/useAuth";
+
+import "../styles/.auth.scss";
+import { FormEvent, useState } from "react";
+import { database } from "../services/firebase";
+
+export function Home() {
+  const history = useHistory();
+  const { signInWithGoogle, user } = useAuth();
+  const [roomCode, setRoomCode] = useState("");
+
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle();
+    }
+    history.push("/rooms/new");
+  }
+
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (roomCode.trim() === "") {
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+    console.log("wololo");
+    if (!roomRef.exists()) {
+      alert("Room does not exists");
+      return;
+    }
+
+    history.push(`/room/${roomCode}`);
+  }
+
+  return (
+    <div id="page-auth">
       <aside>
-        <img src={illustrationImg} alt="Ilustration about questions and answers"/>
+        <img
+          src={illustrationImg}
+          alt="Ilustration about questions and answers"
+        />
         <strong>Crie salas de Q&amp;A ao-vivo</strong>
         <p>Tire as dúvidas da sua audiência em tempo-real</p>
       </aside>
       <main>
-        <div>
-          <img src={logoImg} alt="Pergunta Tchê"/>
-          <button>
-            <img src={googleIconImg} alt="Google image"/>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+          <button onClick={handleCreateRoom} className="create-room">
+            <img src={googleIconImg} alt="Google" />
             Crie sua sala com o google
           </button>
-          <div>
-            ou entre em uma sala
-          </div>
-          <form>
-            <input type="text" placeholder="Digite o código da sala"></input>
-            <button type="submit">Entrar na sala</button>
+          <div className="separator">ou entre em uma sala</div>
+          <form onSubmit={handleJoinRoom}>
+            <input
+              type="text"
+              placeholder="Digite o código da sala"
+              onChange={(event) => setRoomCode(event.target.value)}
+              value={roomCode}
+            ></input>
+            <Button type="submit">Entrar na sala</Button>
           </form>
         </div>
       </main>
     </div>
-  )
+  );
 }
